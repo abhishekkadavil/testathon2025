19d1d4783d01243647112a6520082d7d0787bf61
Abhishek Kadavil <abhishek.kadavil@gmail.com>
2025-11-14 12:32:40 +0530

    Update EmployeeDepartmentSimulation.scala

--- src/test/scala/api/EmployeeDepartmentSimulation.scala
+++ src/test/scala/api/EmployeeDepartmentSimulation.scala
@@ -34,15 +34,15 @@ class EmployeeDepartmentSimulation extends Simulation {
   )
     .protocols(httpProtocol)
   // PASS
-    // .assertions(
-    //   global.successfulRequests.percent.gt(50),
-    //   global.responseTime.mean.lt(5000),
-    //   global.responseTime.percentile4.lt(8000)
-    // )
-  // FAIL
     .assertions(
-        global.successfulRequests.percent.gt(95),        // Pass if >95% succeed
-        global.responseTime.mean.lt(300),                // mean < 300ms
-        global.responseTime.percentile4.lt(800)          // p99 < 800ms
-      )
+      global.successfulRequests.percent.gt(50),
+      global.responseTime.mean.lt(5000),
+      global.responseTime.percentile4.lt(8000)
+    )
+  // FAIL
+    // .assertions(
+    //     global.successfulRequests.percent.gt(95),        // Pass if >95% succeed
+    //     global.responseTime.mean.lt(300),                // mean < 300ms
+    //     global.responseTime.percentile4.lt(800)          // p99 < 800ms
+    //   )
 }
